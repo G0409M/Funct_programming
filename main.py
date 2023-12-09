ec90f338247fce8e4350739c04484ce2d075698a
G0409M <gabrysia.maslanka@gmail.com>
2023-12-09 13:36:39 +0100

    Full Main with writing down func

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import requests
 import pandas as pd
 import tabulate
 from Functions import result, creating_dataframe, write_to_excel
+from Graphic import plot_dataframe
+
 # Część odpowiadająca za pobieranie danych
 dframe = creating_dataframe()
 print("Pobrane dane: ")
 print(dframe.to_markdown(index=False))
 write_to_excel(dframe, "input.xlsx")
+
+
 # Część funkcyjna
 r_dframe = result(dframe)
 print(r_dframe.to_markdown(index=False))
 #Zapisywanie wyników w excelu
 write_to_excel(r_dframe, "result.xlsx")
+
+
 # Część graficzna
+plot_dataframe(r_dframe)
