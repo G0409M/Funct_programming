0aa50b14dcd95c9ce6376fd72dedd19eb5cd2025
G0409M <gabrysia.maslanka@gmail.com>
2023-12-09 10:38:45 +0100

    Verion with more functions

--- Functions.py
+++ Functions.py
@@ -1,6 +1,6 @@
 from functools import reduce
 def result(df):
-    composed_function = compose(filter_dataframe_by_region, filter_dataframe_by_letter_a)
+    composed_function = compose(filter_dataframe_by_subregion,filter_dataframe_by_region, filter_dataframe_by_letter_a)
 
     result_df = composed_function(df)
 
@@ -18,5 +18,10 @@ def filter_dataframe_by_region(dframe):
 def filter_dataframe_by_letter_a(dframe):
     print("wybieranie tylko państw które zawierają literę a")
     filtered_df = dframe[dframe['Country'].str.lower().str.contains('a')]
+    '''print(dframe.to_markdown(index=False))'''
+    return filtered_df
+def filter_dataframe_by_subregion(dframe):
+    print("wybieranie tylko państw które leżą w wschodniej lub środkowej europie lub połudiowej Ameryce")
+    filtered_df = dframe[(dframe['Subregion'] == 'Western Europe') | (dframe['Subregion'] == 'Central Europe') | (dframe['Subregion'] == 'South America') ]
     '''print(dframe.to_markdown(index=False))'''
     return filtered_df
