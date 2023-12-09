dc49060dc6c7f0fc57d4e91e1c9a525863a796f0
G0409M <gabrysia.maslanka@gmail.com>
2023-12-09 10:46:07 +0100

    updated main

--- main.py
+++ main.py
@@ -2,24 +2,19 @@ import requests
 import pandas as pd
 import tabulate
 from Functions import result
+# Część odpowiadająca za pobieranie danych
 def get_country_data(country_code):
     url = f'https://restcountries.com/v3/alpha/{country_code}'
     response = requests.get(url)
     data = response.json()
     return data
-# Lista kodów krajów (możesz dostosować listę według własnych potrzeb)
 country_codes = ['PL', 'US', 'CA', 'DE', 'FR', 'GB', 'IT', 'JP', 'AU', 'BR', 'IN', 'CN', 'RU', 'ZA', 'KR', 'MX',
                  'ES', 'ID', 'NG', 'EG', 'SA', 'AR', 'TR', 'IR', 'TH', 'IT', 'VN', 'PH', 'GB', 'FR',
                  'EG', 'GR', 'NL', 'PT', 'BE', 'SE', 'CH', 'AT', 'NO', 'DK', 'FI', 'IE', 'CL', 'CO', 'VE', 'PE', 'MY', 'SG', 'NZ']
-
-# Inicjalizacja pustej listy do przechowywania danych krajów
 all_countries_data = []
-
-# Pętla pobierająca dane dla każdego kraju
 for country_code in country_codes:
     country_data = get_country_data(country_code)
     all_countries_data.append(country_data)
-# Wyodrębnij potrzebne informacje
 country_info_list = []
 for country_data in all_countries_data:
     country_name = country_data[0]['name']['common']
@@ -31,15 +26,13 @@ for country_data in all_countries_data:
         'capital'] else ''
     country_gini = country_data[0]['gini']['2018'] if 'gini' in country_data[0] and '2018' in country_data[0][
         'gini'] else ''
-
     country_info_list.append(
         [country_name, country_population, country_region, country_subregion, country_area, country_capital,
          country_gini])
-
-# Utwórz DataFrame
 columns = ['Country', 'Population', 'Region', 'Subregion', 'Area', 'Capital', 'Gini']
 dframe = pd.DataFrame(country_info_list, columns=columns)
+print("Pobrane dane: ")
 print(dframe.to_markdown(index=False))
-print("tu jestem 1")
+# Część funkcyjna
 result(dframe)
-print("tu jestem 2")
+# Część graficzna
