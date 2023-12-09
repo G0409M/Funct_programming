52599aa74693615ded5e5613ae894a53f3d29da2
G0409M <gabrysia.maslanka@gmail.com>
2023-12-09 10:11:53 +0100

    versio with no returns

--- Functions.py
+++ Functions.py
@@ -1,6 +1,6 @@
 from functools import reduce
 def result(df):
-    compose(filter_dataframe_by_region(df))
+    compose(filter_dataframe_by_letter_a(df),filter_dataframe_by_region(df))
 
 
 def compose(*funcs):
@@ -9,5 +9,11 @@ def compose(*funcs):
 def filter_dataframe_by_region(dframe):
     print("wybieranie tylko regionu EUROPE")
     filtered_df = dframe[dframe['Region'] == 'Europe']
+    dframe= filtered_df
+    print(dframe.to_markdown(index=False))
+
+def filter_dataframe_by_letter_a(dframe):
+    print("wybieranie tylko państw które zawierają literę a")
+    filtered_df = dframe[dframe['Country'].str.lower().str.contains('a')]
     dframe= filtered_df
     print(dframe.to_markdown(index=False))
